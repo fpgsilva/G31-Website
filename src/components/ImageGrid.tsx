2b79ded2e221a6d2f9f31f17f8bbff27b4f12f88
Rodrigo costa <re.costa@campus.fct.unl.pt>
2024-11-09 15:16:22 +0000

    report 3 e bom e maus desings

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -7,22 +7,25 @@ const ImageGrid = () => {
       name: "Diogo Pinto",
       imageUrl: "FotoDoCidadao_-_copia.jpg",
       number: "67535",
+      goodDesign: "Good_and_Bad_design_67535.pdf",
     },
     {
       name: "Francisco Silva",
-      imageUrl:
-        "foto_FS.jpg",
+      imageUrl: "foto_FS.jpg",
       number: "70525",
+      goodDesign: "cenas cool yh do chico pdf",
     },
     {
       name: "Martim Latas",
       imageUrl: "foto_ML.png",
       number: "70596",
+      goodDesign: "Good_Bad_Design_Martim_Latas_70596.pdf",
     },
     {
       name: "Rodrigo Costa",
       imageUrl: "trabalho-de-ipm.jpeg",
       number: "63703",
+      goodDesign: "BOM DESING_MAU DESING_Rodrigo.pdf",
     },
   ];
 
@@ -37,10 +40,14 @@ const ImageGrid = () => {
       imageImport: "Cover-Report-2.png",
       file: "IPM-G31-Phase 2.pdf",
     },
+    {
+      title: "Report 3",
+      imageImport: "Cover-Report-3.png",
+      file: "IPM-G31-Phase 3.pdf",
+    },
   ];
 
   return (
-    
     <div className="page-container">
       {/* Logo at the top */}
       <div className="logo-container">
@@ -50,13 +57,13 @@ const ImageGrid = () => {
           className="logo"
         />
       </div>
-      
+
       <div className="arrow-container">
-          <i className="arrow"/>
+        <i className="arrow" />
       </div>
 
       <div>
-        <ImageSlider/>
+        <ImageSlider />
       </div>
 
       {/* Group name */}
@@ -87,6 +94,21 @@ const ImageGrid = () => {
             />
             <p className="aluno-description">{aluno.name}</p>
             <p className="aluno-description">{aluno.number}</p>
+            <p className="aluno-design">Good Design Bad Design</p>
+            <div className="design-buttons">
+              {/* Preview and Download buttons for Good Design Bad Design */}
+              <a
+                href={aluno.goodDesign}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="preview-button"
+              >
+                Preview
+              </a>
+              <a href={aluno.goodDesign} download className="download-button">
+                Download
+              </a>
+            </div>
           </div>
         ))}
       </div>
@@ -102,10 +124,10 @@ const ImageGrid = () => {
             />
             <p></p>
             {/* Preview button */}
-            <a 
-              href={report.file} 
-              target="_blank" 
-              rel="noopener noreferrer" 
+            <a
+              href={report.file}
+              target="_blank"
+              rel="noopener noreferrer"
               className="preview-button"
             >
               Preview Report {index + 1}
